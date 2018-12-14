364db211dce07e88974ce36b6441212d2c60fc48
Luca Prete <luca@onlab.us>
2018-12-14 10:12:32 -0800

    Fix helmrepo script syntax
    Change-Id: I3d488855232c9e94959d3e32c67c0b37c7e150f1

--- helmrepo.sh
+++ helmrepo.sh
@@ -43,7 +43,7 @@ done < <(find "${WORKSPACE}" -name Chart.yaml -print0)
 
 echo "Generating repo index"
 
-helm repo index "${REPO_DIR}" --url "${PUBLISH_URL}" --merge "${ORIGINAL_INDEX_YAML}
+helm repo index "${REPO_DIR}" --url "${PUBLISH_URL}" --merge "${ORIGINAL_INDEX_YAML}"
 
 echo "Finished, chart repo generated: ${REPO_DIR}"
 
